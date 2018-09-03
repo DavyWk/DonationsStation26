271345417ea5bdd75748745608e9c60bdcd15374
Davy.W <cnetadev@hotmail.fr>
2018-09-03 16:38:15 -0400

    Began working on P1

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Moetchadjia Davy Waku Kouomou
  *  @version 1.1
  */
 public class Person1 {

174ac66829b7fcb3aefac18bd7f417cbbd60a7b6
McKennley Wilson <33469404+Mack33467@users.noreply.github.com>
2018-09-07 16:19:53 -0400

    Changed AntLabMain

---
+++ M2/readme.p5.txt
@@ -0,0 +1,4 @@
+There's more than enough room for both cowboys in the town.
+
+McKennley Wilson
+mwilson317@gatech.edu
--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -73,8 +73,8 @@ import edu.gatech.oad.antlab.pkg2.*;
                   toPrint += p4.toString("gburdell4");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("P5");
-                  toPrint += p5.toString("gburdell5");
+                  Person5 p5 = new Person5("McKennley Wilson");
+                  toPrint += p5.toString("mwilson317@gatech.edu");
 		  
         System.out.println(toPrint);
         

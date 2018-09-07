f8bfcc3c85aeb4ac0eb81225c2decb98df8a0429
McKennley Wilson <33469404+Mack33467@users.noreply.github.com>
2018-09-07 16:35:16 -0400

    Done everybody

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -74,7 +74,7 @@ import edu.gatech.oad.antlab.pkg2.*;
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
                   Person5 p5 = new Person5("McKennley Wilson");
-                  toPrint += p5.toString("mwilson317@gatech.edu");
+                  toPrint += p5.toString("mwilson317");
 		  
         System.out.println(toPrint);
         

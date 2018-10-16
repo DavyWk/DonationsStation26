3a79214af526bf16fbf5f24329757db1fba59930
Liana Syrkett <lianasyrkett424@gmail.com>
2018-10-16 17:44:16 -0400

    Revert "Location Additions"
    This reverts commit e0be1f104731fbc233c4b94c5742a3076adb6a23.

--- app/src/main/java/edu/gatech/ds26/model/LocationList.java
+++ app/src/main/java/edu/gatech/ds26/model/LocationList.java
@@ -1,16 +1,24 @@
 package edu.gatech.ds26.model;
 
+import java.util.List;
 import java.util.ArrayList;
-
+//TODO: read file + io
 public class LocationList {
 
     private static final LocationList _instance = new LocationList();
     public static LocationList getInstance() { return _instance; }
 
-    private ArrayList<Location> locationList;
+    private List<Location> locationList;
 
     private LocationList() {
         locationList = new ArrayList<>();
+    }
+    //add location
+    //remove
+
+    public boolean addLocation(String locationString) {
+        Location loc = parseLocation(locationString);
+        return addLocation(loc);
     }
 
     public boolean addLocation(Location loc) {
@@ -21,7 +29,28 @@ public class LocationList {
         return true;
     }
 
-    public ArrayList<Location> get() {
+    public Location parseLocation(String locationString) {
+        String[] split = locationString.split(",");
+        if (split.length != 11) {
+            return null;
+        }
+        int key = Integer.parseInt(split[0]);
+        String name = split[1];
+        double latitude = Double.parseDouble(split[2]);
+        double longitude = Double.parseDouble(split[3]);
+        String address = split[4];
+        String city = split[5];
+        String state = split[6];
+        int zip = Integer.parseInt(split[7]);
+        String type = split[8];
+        String phone = split[9];
+        String website = split[10];
+
+        return new Location(key, name, latitude, longitude, address, city, state,
+                zip, type, phone, website);
+    }
+
+    public List<Location> get() {
         return locationList;
     }
 }
--- app/src/main/java/edu/gatech/ds26/model/UserList.java
+++ app/src/main/java/edu/gatech/ds26/model/UserList.java
@@ -15,7 +15,7 @@ public class UserList {
     private UserList() {
         userList = new ArrayList<>();
         userList.add(new User("user", "user1", "1"));
-        userList.add(new Admin("admin", "admin1,", "1"));
+        userList.add(new Admin("admin", "admin1,", "1")); // Admin is-a User
     }
 
     public boolean authenticateUser(String loginId, String password) {

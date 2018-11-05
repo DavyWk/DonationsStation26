c872a2fd6b7aa73e7d0ee6728b2c0a7a7b59846a
Davy.W <davy.w@outlook.com>
2018-11-05 08:45:33 -0500

    Fixed formatting

--- app/src/main/java/edu/gatech/ds26/controllers/DonationAllActivity.java
+++ app/src/main/java/edu/gatech/ds26/controllers/DonationAllActivity.java
@@ -8,7 +8,7 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import edu.gatech.ds26.R;
 import edu.gatech.ds26.model.Donation;
@@ -16,7 +16,7 @@ import edu.gatech.ds26.model.DonationList;
 
 public class DonationAllActivity extends AppCompatActivity {
 
-    ArrayList<Donation> donations;
+    List<Donation> donations;
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager layoutManager;
@@ -26,7 +26,7 @@ public class DonationAllActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donation_all);
-        recyclerView = (RecyclerView) findViewById(R.id.recyclerViewDonation);
+        recyclerView = findViewById(R.id.recyclerViewDonation);
 
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);

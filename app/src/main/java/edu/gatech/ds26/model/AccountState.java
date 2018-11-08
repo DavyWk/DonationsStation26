d7161e635c7e5b7bf41f60cd5a6bb8e3381ebf24
McKennley Wilson <33469404+Mack33467@users.noreply.github.com>
2018-11-07 21:50:32 -0500

    Adding a few javadocs

--- app/src/main/java/edu/gatech/ds26/model/AccountState.java
+++ app/src/main/java/edu/gatech/ds26/model/AccountState.java
@@ -1,13 +1,24 @@
 package edu.gatech.ds26.model;
 
+/**
+ * This is a public enum class that indicates the current state of a user
+ * Currently the security feature is unimplemented
+ * V 1.0
+ */
 public enum AccountState {
     LOCKED,
     UNLOCKED;
 
+    @Override
     public String toString() {
         return name().charAt(0) + name().substring(1).toLowerCase();
     }
 
+    /**
+     *
+     * @param s A string parameter
+     * @return Returns whether the indicated account is locked or unlocked
+     */
     public static AccountState get(String s) {
         return AccountState.valueOf(s.toUpperCase());
     }

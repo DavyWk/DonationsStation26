24d4862b09ea3cbd1024ed001cbb221b7c94afcf
McKennley Wilson <33469404+Mack33467@users.noreply.github.com>
2018-11-15 22:21:45 -0500

    UserListTest
    Tests the authenticateUser in UserList

---
+++ app/src/test/java/edu/gatech/ds26/model/UserListTest.java
@@ -0,0 +1,90 @@
+package edu.gatech.ds26.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+    @author McKennley Wilson
+    Checks the authenticate user method
+ */
+public class UserListTest {
+
+    private UserList userList;
+    private User user;
+
+    /**
+     * Initializes an instance of the userList and a user
+     */
+    @Before
+    public void setUp() {
+        userList = UserList.getInstance();
+        user = new User("Bob", "Water1","2340");
+        assertTrue(userList.addUser(user));
+    }
+
+    /**
+     * Tests authenticateUser with an unregistered user
+     * have been input
+     */
+    @Test
+    public void UnregisteredUser() {
+        boolean test = userList.authenticateUser("Billy","hacker1");
+        assertFalse(test);
+    }
+
+    /**
+     * Tests authenticateUser with a correct user and incorrect password
+     * have been input
+     */
+    @Test
+    public void RegisteredUserWrongPassword() {
+        boolean test = userList.authenticateUser("Water1","hacker1");
+        assertFalse(test);
+    }
+
+    /**
+     * Tests authenticateUser with a right user and right password
+     * have been input
+     */
+    @Test
+    public void RegisteredUserRightPassword() {
+        boolean test = userList.authenticateUser("Water1","2340");
+        assertTrue(test);
+    }
+
+    /**
+     * Tests authenticateUser with a null user and password
+     * have been input
+     */
+    @Test
+    public void NullUserNullPassword() {
+        boolean test = userList.authenticateUser(null,null);
+        assertFalse(test);
+    }
+
+    /**
+     * Tests authenticateUser with an unregistered user and null password
+     * have been input
+     */
+    @Test
+    public void UnregisteredUserNullPassword() {
+        boolean test = userList.authenticateUser("CodeBreaker",null);
+        assertFalse(test);
+    }
+
+    /**
+     * Tests authenticateUser with an unregistered user and null password
+     * have been input
+     */
+    @Test
+    public void RegisteredUserNullPassword() {
+        boolean test = userList.authenticateUser("Water1",null);
+        assertFalse(test);
+    }
+
+}
